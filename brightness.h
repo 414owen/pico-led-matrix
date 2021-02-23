63343593bd71a4b21f0ab17e9ec5e06bc6bd7fd6
Owen Shepherd <owen@owen.cafe>
2021-02-23 23:15:15 +0000

    added brightness levels

---
+++ brightness.h
@@ -0,0 +1,6 @@
+#ifndef brightness_h_INCLUDED
+#define brightness_h_INCLUDED
+
+bool is_led_on(int brightness, int tick);
+
+#endif // brightness_h_INCLUDED
--- main.c
+++ main.c
@@ -1,9 +1,16 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "pico/bootrom.h"
 #include "pico/stdlib.h"
 #include "pico/multicore.h"
+
+#include "brightness.h"
+
 #define ROWS 2
 #define COLS 2
+
+extern const uint16_t brightnesses[];
+extern const int MAX_BRIGHTNESS;
 
 const int BUFSIZE = 128;
 const int zero = 0;
@@ -13,7 +20,7 @@ typedef struct led {
   uint y;
 } led;
 
-bool led_states[ROWS][COLS] = { { 0, 0 }, { 0, 0 } };
+uint8_t led_states[ROWS][COLS] = { { 0, 8 }, { 8, 0 } };
 
 void reboot(void) {
   reset_usb_boot(25, 0);
@@ -39,15 +46,17 @@ void print_state(void) {
 }
 
 void display_leds(void) {
+  uint8_t tick = 0;
   while (true) {
     for (uint i = 0; i < ROWS; i++) {
       gpio_put(i, 1);
       for (uint j = 0; j < COLS; j++) {
-        gpio_put(COLS + j, !led_states[i][j]);
+        gpio_put(COLS + j, !is_led_on(led_states[i][j], tick));
       }
-      sleep_ms(1);
+      sleep_us(500);
       gpio_put(i, 0);
     }
+    tick = (tick + 1) % MAX_BRIGHTNESS;
   }
 }
 
@@ -86,21 +95,37 @@ void toggle_leds_from_input(void) {
   }
 }
 
-void set_leds_to(bool on, int delay) {
+void set_leds_to(int to, int delay) {
   for (int i = 0; i < ROWS * COLS; i++) {
     int j = (i + 1) % (ROWS * COLS);
-    led_states[j / COLS][j % COLS] = on;
-    sleep_ms(delay);
+    uint8_t prev = led_states[j / COLS][j % COLS];
+    int add = to > prev ? 1 : -1;
+    for (int k = 0; k < abs(to - prev); k++) {
+      led_states[j / COLS][j % COLS] += add;
+      sleep_ms(delay / 100);
+    }
   }
 }
 
 void led_pattern(void) {
   while (true) {
-    set_leds_to(1, 250);
-    sleep_ms(250);
-    set_leds_to(0, 500);
-    sleep_ms(250);
+    for (int i = 0; i < ROWS; i++) {
+      for (int j = 0; j < COLS; j++) {
+        led_states[i][j] += 1;
+        led_states[i][j] %= MAX_BRIGHTNESS;
+        printf(" %d", led_states[i][j]);
+      }
+    }
+    putchar('\n');
+    handle_quit_input(getchar_timeout_us(100));
+    sleep_ms(500);
+  }
+}
+
+void spin_for_reset(void) {
+  while (true) {
     handle_quit_input(getchar_timeout_us(1000));
+    sleep_ms(100);
   }
 }
 
@@ -117,5 +142,6 @@ int main(void) {
   countdown();
   multicore_launch_core1(display_leds);
   led_pattern();
+  // spin_for_reset();
   return 0;
 }
