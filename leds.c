09a42fef574c1948b723df8bb302e694693086a2
Owen Shepherd <owen@owen.cafe>
2021-02-24 14:33:21 +0000

    added matrix code

---
+++ leds.c
@@ -0,0 +1,4 @@
+int positives[] = { 13, 14, 15, 16, 17 };
+int negatives[] = { 18, 19, 20, 21, 22, 26, 27, 28};
+int positive_num = sizeof(positives) / sizeof(positives[0]);
+int negative_num = sizeof(negatives) / sizeof(negatives[0]);
--- main.c
+++ main.c
@@ -11,16 +11,15 @@
 
 extern const uint16_t brightnesses[];
 extern const int MAX_BRIGHTNESS;
+extern const int positives[];
+extern const int negatives[];
+extern const int positive_num;
+extern const int negative_num;
 
 const int BUFSIZE = 128;
 const int zero = 0;
 
-typedef struct led {
-  uint x;
-  uint y;
-} led;
-
-uint8_t led_states[ROWS][COLS] = { { 0, 8 }, { 8, 0 } };
+uint8_t led_states[ROWS][COLS] = { { 16, 16 }, { 16, 16 } };
 
 void reboot(void) {
   reset_usb_boot(25, 0);
@@ -30,118 +29,31 @@ void handle_quit_input(char c) {
   if (c == 'r') reboot();
 }
 
-void io_debug(uint g, bool state) {
-  printf("%d %d\n", g, state);
-  gpio_put(g, state);
-}
-
-void print_state(void) {
-  printf("leds on:");
-  for (int i = 0; i < ROWS; i++) {
-    for (int j = 0; j < COLS; j++) {
-      if (led_states[i][j]) printf(" %d", i * COLS + j);
-    }
-  }
-  printf("\n");
-}
-
-void display_leds(void) {
-  uint8_t tick = 0;
-  while (true) {
-    for (uint i = 0; i < ROWS; i++) {
-      gpio_put(i, 1);
-      for (uint j = 0; j < COLS; j++) {
-        gpio_put(COLS + j, !is_led_on(led_states[i][j], tick));
-      }
-      sleep_us(500);
-      gpio_put(i, 0);
-    }
-    tick = (tick + 1) % MAX_BRIGHTNESS;
-  }
-}
-
-// so we have time to connect to serial
-void countdown(void) {
-  for (int i = 30; i >= 0; i--) {
-    printf("countdown %d\n", i);
-    sleep_ms(100);
-  }
-}
-
-int get_int_stdin(void) {
-  while (true) {
-    int res = getchar();
-    handle_quit_input(res);
-    return res - '0';
-  }
-}
-
-int get_led_ind_stdin(void) {
-  while (true) {
-    int res = get_int_stdin();
-    if (res >= 0 && res < ROWS * COLS) return res;
-  }
-}
-
-void toggle_leds_from_input(void) {
-  while (true) {
-    printf("> ");
-    fflush(stdout);
-    int led_ind = get_led_ind_stdin();
-    printf("got input: %d\n", led_ind);
-    led_states[led_ind / COLS][led_ind % COLS]
-      = !led_states[led_ind / COLS][led_ind % COLS];
-    print_state();
-  }
-}
-
-void set_leds_to(int to, int delay) {
-  for (int i = 0; i < ROWS * COLS; i++) {
-    int j = (i + 1) % (ROWS * COLS);
-    uint8_t prev = led_states[j / COLS][j % COLS];
-    int add = to > prev ? 1 : -1;
-    for (int k = 0; k < abs(to - prev); k++) {
-      led_states[j / COLS][j % COLS] += add;
-      sleep_ms(delay / 100);
-    }
-  }
-}
-
 void led_pattern(void) {
   while (true) {
-    for (int i = 0; i < ROWS; i++) {
-      for (int j = 0; j < COLS; j++) {
-        led_states[i][j] += 1;
-        led_states[i][j] %= MAX_BRIGHTNESS;
-        printf(" %d", led_states[i][j]);
-      }
+    for (int i = 0; i < positive_num; i++) {
+      gpio_put(positives[i], 1);
+      handle_quit_input(getchar_timeout_us(100));
+      sleep_ms(500);
+      gpio_put(positives[i], 0);
     }
-    putchar('\n');
-    handle_quit_input(getchar_timeout_us(100));
-    sleep_ms(500);
-  }
-}
-
-void spin_for_reset(void) {
-  while (true) {
-    handle_quit_input(getchar_timeout_us(1000));
-    sleep_ms(100);
   }
 }
 
 void init_pins(void) {
-  for (int i = 0; i < 4; i++) {
-    gpio_init(i);
-    gpio_set_dir(i, GPIO_OUT);
+  for (int i = 0; i < positive_num; i++) {
+    gpio_init(positives[i]);
+    gpio_set_dir(positives[i], GPIO_OUT);
+  }
+  for (int i = 0; i < negative_num; i++) {
+    gpio_init(negatives[i]);
+    gpio_set_dir(negatives[i], GPIO_OUT);
   }
   stdio_init_all();
 }
 
 int main(void) {
   init_pins();
-  countdown();
-  multicore_launch_core1(display_leds);
   led_pattern();
-  // spin_for_reset();
   return 0;
 }
